0e727ae7df13a139bce80cdec01bd7859357b594
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-07-20 10:25:05 +0530

    try to merge

--- orbitz_clone/src/Pages/Product/Productpage.jsx
+++ orbitz_clone/src/Pages/Product/Productpage.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Productpage = () => {
   return (
     <div>
-        <h1>Product Productpage</h1>
+        <h1>Product Productpage
+            <p>change branch</p>
+        </h1>
     </div>
   )
 }
