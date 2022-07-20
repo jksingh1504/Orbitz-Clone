9f786fa2cfba7968dd7eef9a40a0553b835621d8
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-07-20 11:16:09 +0530

    ,

--- orbitz_clone/src/Components/MainRoutes.jsx
+++ orbitz_clone/src/Components/MainRoutes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { Searchbar } from '../Pages/Product/Searchbar'
 
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<h1 style={{textAlign:"center"}}>Orbitz Clone</h1>}/>
+        <Searchbar/>
     </Routes>
   )
 }
