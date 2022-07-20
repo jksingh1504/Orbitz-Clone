f7875e57cf2c230ac08ebe53095f2e0cf03c2e34
Roshan Arikar <roshan.g.arikar@gmail.com>
2022-07-20 11:28:11 +0530

    create login page

--- orbitz_clone/src/Components/MainRoutes.jsx
+++ orbitz_clone/src/Components/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
+import { Login } from '../Pages/Login/Login'
 
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
     </Routes>
   )
 }
