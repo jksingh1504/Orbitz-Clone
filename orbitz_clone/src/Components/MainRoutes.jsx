e4e6f2908fff6bb774df73031ac91676c455e0f8
Roshan Arikar <roshan.g.arikar@gmail.com>
2022-07-21 16:56:35 +0530

    sign up path change

--- orbitz_clone/src/Components/MainRoutes.jsx
+++ orbitz_clone/src/Components/MainRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
 import { Login } from '../Pages/Login/Login'
+import { Signup } from '../Pages/Signup/Signup'
 import IndiProduct from "../Pages/IndividualProduct/IndiProduct"
 
 
