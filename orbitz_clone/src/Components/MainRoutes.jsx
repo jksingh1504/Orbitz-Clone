fd2978c48e7a65360384ec502040b6c7920cafae
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-07-23 17:13:22 +0530

    sort

--- orbitz_clone/src/Components/MainRoutes.jsx
+++ orbitz_clone/src/Components/MainRoutes.jsx
@@ -4,12 +4,13 @@ import Home from '../Pages/Home'
 import { Login } from '../Pages/Login/Login'
 import { Signup } from '../Pages/Signup/Signup'
 import IndiProduct from "../Pages/IndividualProduct/IndiProduct"
+import Productpage from '../Pages/Product/Productpage'
 
 
 const MainRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<IndiProduct/>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
     </Routes>
