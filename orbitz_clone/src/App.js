e5eedd072a9eec0ca82d4bca11244e81301ad00c
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-07-23 09:52:54 +0530

    product

--- orbitz_clone/src/App.js
+++ orbitz_clone/src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import { LandingPage } from './Components/Index/LandingPage';
 import MainRoutes from './Components/MainRoutes';
 import Home from './Pages/Home';
-
+import Productpage from "./Pages/Product/Productpage"
 function App() {
   return (
     <>
     {/* <MainRoutes/> */}
-  <LandingPage/>
-    
+  {/* <LandingPage/> */}
+    <Productpage/>
     </>
   );
 }
